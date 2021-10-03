cca682ac68b67462103bf1e5fee5c70a81b169f6
Abdulahi Jama <kararjaamac1@gmail.com>
2021-10-03 13:52:14 -0700

    updated hello world

--- src/Tests/HelloWorldTest.java
+++ src/Tests/HelloWorldTest.java
@@ -3,5 +3,7 @@ package Tests;
 public class HelloWorldTest {
     public static void main(String[] args) {
         System.out.println("hello world ");
+
+        System.out.println("one more hello world");
     }
 }
